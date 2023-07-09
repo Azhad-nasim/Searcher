8e60a425bb91092c5f34916752f7500f6094ea48
AZHAD <azhadnasim.786@gmail.com>
2023-07-09 17:02:11 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,204 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").css("top", "0px");
+    } else {
+      $(".sticky-top").css("top", "-100px");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
+
+// Fake Candidate List
+
+function searchCandidates() {
+  var location = document.getElementById("location").value;
+  var jobRole = document.getElementById("jobRole").value;
+  // Perform candidate search and populate the list dynamically
+  var candidates = getCandidates(location, jobRole);
+  var candidatesList = document.getElementById("candidates");
+  candidatesList.innerHTML = "";
+  candidates.forEach(function (candidate) {
+    var li = document.createElement("li");
+    li.textContent = candidate.name;
+    candidatesList.appendChild(li);
+    li.classList.add("text-secondary");
+  });
+}
+
+// Dummy function to simulate candidate data retrieval
+function getCandidates(location, jobRole) {
+  // Perform API request or fetch data from a database
+  // Return array of candidates based on location and job role
+  return [
+    { name: "John Doe" },
+    { name: "Jane Smith" },
+    { name: "David Johnson" },
+  ];
+}
+
+//Login Form
+const labels = document.querySelectorAll("#form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 100}ms">${letter}</span>`
+    )
+    .join("");
+});
+
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function () {
+  if (animating) return false;
+  animating = true;
+
+  current_fs = $(this).parent();
+  next_fs = $(this).parent().next();
+
+  //activate next step on progressbar using the index of next_fs
+  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+  //show the next fieldset
+  next_fs.show();
+  //hide the current fieldset with style
+  current_fs.animate(
+    { opacity: 0 },
+    {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2;
+        //2. bring next_fs from the right(50%)
+        left = now * 50 + "%";
+        //3. increase opacity of next_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({
+          transform: "scale(" + scale + ")",
+          position: "absolute",
+        });
+        next_fs.css({ left: left, opacity: opacity });
+      },
+      duration: 800,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: "easeInOutBack",
+    }
+  );
+});
+
+$(".previous").click(function () {
+  if (animating) return false;
+  animating = true;
+
+  current_fs = $(this).parent();
+  previous_fs = $(this).parent().prev();
+
+  //de-activate current step on progressbar
+  $("#progressbar li")
+    .eq($("fieldset").index(current_fs))
+    .removeClass("active");
+
+  //show the previous fieldset
+  previous_fs.show();
+  //hide the current fieldset with style
+  current_fs.animate(
+    { opacity: 0 },
+    {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale previous_fs from 80% to 100%
+        scale = 0.8 + (1 - now) * 0.2;
+        //2. take current_fs to the right(50%) - from 0%
+        left = (1 - now) * 50 + "%";
+        //3. increase opacity of previous_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({ left: left });
+        previous_fs.css({
+          transform: "scale(" + scale + ")",
+          opacity: opacity,
+        });
+      },
+      duration: 800,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: "easeInOutBack",
+    }
+  );
+});
